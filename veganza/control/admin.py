a076b854ba9ae707007ba8e1a0a0c979e454978e
shivamkshirsagar11 <shivamkshirsagar2002@gmail.com>
2022-05-11 18:03:24 +0530

    basic package created

--- veganza/control/admin.py
+++ veganza/control/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from control.models import Login,Movies
 # Register your models here.
+admin.site.register(Login)
+admin.site.register(Movies)
--- veganza/control/models.py
+++ veganza/control/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Login(models.Model):
+    email = models.CharField(max_length=255,null=False)
+    password = models.CharField(max_length=12,null=False)
+    last_visit = models.DateTimeField()
+
+class Movies(models.Model):
+    name = models.CharField(max_length=255)
+    year = models.CharField(max_length=4)
+    actors = models.TextField(max_length=1028)
+    category = models.TextField(max_length=1028)
+    synopsis = models.TextField(max_length=11028)
+    poster = models.ImageField(upload_to="movie_poster",default="movie_poster/default.jpg")
