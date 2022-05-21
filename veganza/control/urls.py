7afd77dc92187862537eb3df86277f5ccb0fdd63
shivamkshirsagar11 <shivamkshirsagar2002@gmail.com>
2022-05-21 09:57:54 +0530

    like dislike watchlist all actions completed

--- veganza/control/urls.py
+++ veganza/control/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.index,name='Lost_Home'),
     path('addto', views.addto,name='addto'),
+    path('like', views.like,name='like'),
+    path('dislike', views.dislike,name='dislike'),
+    path('watch', views.watch,name='watch'),
+    path('reset', views.reset,name='reset'),
 ]
--- veganza/control/views.py
+++ veganza/control/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from control.models import Movies as m,Like,Dislike,Favorite
 from django.views.decorators.csrf import csrf_exempt
 
@@ -11,20 +11,88 @@ def addto(request):
     if request.method == 'POST':
         type = request.POST['type']
         movie = request.POST['mid']
-        obj = m.objects.get(id = movie)
+        print(type,movie)
         if type == 'like':
+            obj = m.objects.get(id = movie)
             like = Like(name = obj.name,mid = obj.id)
             obj.like = True
+            if obj.dislike:
+                obj.dislike = False
+                t = Dislike.objects.get(mid = obj.id)
+                t.delete()
             obj.save()
             like.save()
         elif type == 'dislike':
+            obj = m.objects.get(id = movie)
             dislike = Dislike(name = obj.name,mid = obj.id)
             obj.dislike = True
+            if obj.like:
+                obj.like = False
+                t = Like.objects.get(mid = obj.id)
+                t.delete()
             obj.save()
             dislike.save()
         elif type == 'watch':
+            obj = m.objects.get(id = movie)
             fav = Favorite(name = obj.name,mid = obj.id)
             obj.watch = True
             obj.save()
             fav.save()
+        elif type == 'dislike->like':
+            obj = m.objects.get(id = movie)
+            d_l = Dislike.objects.get(mid = obj.id)
+            obj.like = True
+            obj.dislike = False
+            newobj = Like(name = obj.name, mid = obj.id)
+            newobj.save()
+            obj.save()
+            d_l.delete()
+        elif type == 'like->dislike':
+            obj = m.objects.get(id = movie)
+            d_l = Dislike.objects.get(mid = obj.id)
+            obj.like = False
+            obj.dislike = True
+            newobj = Dislike(name = obj.name, mid = obj.id)
+            newobj.save()
+            obj.save()
+            d_l.delete()
+        elif type == 'watch->remove':
+            obj = m.objects.get(id = movie)
+            w_r = Favorite.objects.get(mid = obj.id)
+            obj.watch = False
+            obj.save()
+            w_r.delete()
         return HttpResponse("done")
+def like(request):
+    objs = Like.objects.all()
+    movies = []
+    for i in objs:
+        movies.append(m.objects.get(id = i.mid))
+    return render(request, 'like.html',{'movies':movies})
+def dislike(request):
+    objs = Dislike.objects.all()
+    movies = []
+    for i in objs:
+        movies.append(m.objects.get(id = i.mid))
+    return render(request, 'dislike.html',{'movies':movies})
+def watch(request):
+    objs = Favorite.objects.all()
+    movies = []
+    for i in objs:
+        movies.append(m.objects.get(id = i.mid))
+    return render(request, 'watch.html',{'movies':movies})
+
+def reset(request):
+    favobj = Favorite.objects.all()
+    likeobj = Like.objects.all()
+    dislikeobj = Dislike.objects.all()
+    mobj = m.objects.all()
+    for movie in mobj:
+        movie.like = False
+        movie.dislike = False
+        movie.watch = False
+        movie.save()
+    favobj.delete()
+    likeobj.delete()
+    dislikeobj.delete()
+    return redirect('/')
